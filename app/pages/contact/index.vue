6f427f468d1461d1770610a2be7424b7020ec7c8
Vukovic Nikola <admin@nikolav.rs>
2026-07-28 19:31:28 +0200

    page:contact --start

--- app/pages/contact/index.vue
+++ app/pages/contact/index.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { useDisplay } from "vuetify";
+import { useToggleFlag } from "~/composables/utils/use-toggle-flag";
 
 definePageMeta({
   context: {
@@ -26,6 +27,8 @@ const MaxW_AppBoxContainerCentered = $$.config<number>(
   "layout.component.AppBoxContainerCentered.maxWidth",
 )!;
 
+const toggleOverlayForm = useToggleFlag();
+
 // @@eos
 </script>
 
@@ -34,6 +37,7 @@ const MaxW_AppBoxContainerCentered = $$.config<number>(
     <AppBoxContainerCentered
       :class="{ 'mt-2': MaxW_AppBoxContainerCentered < d.width.value }"
     >
+      <!-- kartica; contact-us -->
       <VCard tile>
         <VImg src="@/assets/media/phone-laptop.01.jpg" :height="322" cover>
           <VSheet
@@ -41,7 +45,7 @@ const MaxW_AppBoxContainerCentered = $$.config<number>(
             color="rgba(var(--v-theme-primary-variant), .88)"
             class="!text-v-on-primary backdrop-blur-sm text-shadow-sm"
           >
-            <VCardTitle class="text-center position-relative">
+            <VCardTitle class="text-center">
               <h2>
                 {{ t("Kontaktirajte nas") }}
               </h2></VCardTitle
@@ -64,19 +68,99 @@ const MaxW_AppBoxContainerCentered = $$.config<number>(
             </VCardText>
           </VSheet>
         </VImg>
-        <IconX
-          icon="mdi:phone-in-talk"
-          class="opacity-20 d-block mx-auto mt-5"
-          size="2rem"
-        />
-        <VCardText class="text-body-1 text-justify indent-4">
+
+        <!-- contact text -->
+        <VCardText class="text-body-1 text-justify indent-4 pt-5">
           {{
             t(
-              "Imate ideju, ali ne znate kako da je realizujete? Niste sigurni u dimenzije ili materijal? Nema problema - mi smo tu da vas usmerimo! Slobodno nam se obratite za savet, razmenu ideja ili prvu procenu. Pošaljite upit danas i krenimo sa realizacijom!",
+              "Imate ideju, ali ne znate kako da je realizujete? Niste sigurni u dimenzije ili materijal? Nema problema - mi smo tu da vas usmerimo! Slobodno nam se obratite za savet, razmenu ideja ili prvu procenu. Pozovite nas ili nam pošaljite upit danas i krenimo sa realizacijom!",
             )
           }}
         </VCardText>
+
+        <!-- buttons; cta -->
+        <VCardActions
+          class="pa-3"
+          :class="{
+            'flex-col': !d.smAndUp.value,
+            'justify-around': d.smAndUp.value,
+          }"
+        >
+          <VBtn href="tel:+38163316521" size="x-large">
+            <template #prepend>
+              <IconX icon="mdi:phone" size="1.5rem" />
+            </template>
+            Pozovite nas
+          </VBtn>
+          <VBtn size="x-large" @click="toggleOverlayForm">
+            <template #prepend>
+              <IconX icon="mdi:pencil" size="1.5rem" />
+            </template>
+
+            Pošaljite upit</VBtn
+          >
+        </VCardActions>
+        <AppOverlayScreen v-model="toggleOverlayForm.isActive.value">
+          <AppBoxBase class="text-center">
+            <AppVBtn rounded="full" icon @click="toggleOverlayForm.off">
+              <IconX icon="$close" />
+            </AppVBtn>
+            <h2>overlay:contact-form 🚧</h2>
+          </AppBoxBase>
+        </AppOverlayScreen>
       </VCard>
+
+      <VSpacer class="mt-12" />
+
+      <!-- kontakti -->
+      <AppBoxBase class="space-y-5 text-center">
+        <AppBoxBase>
+          <VCardTitle
+            ><IconX
+              icon="mdi:map-marker"
+              size="2rem"
+              class="text-primary -translate-y-[2px]"
+            />
+            {{ t("Adresa") }}
+          </VCardTitle>
+          <VCardText class="text-body-1"
+            ><NuxtLink
+              href="https://maps.app.goo.gl/RKmTxP7zQXRBTM6M7"
+              external
+              target="_blank"
+              >Dositejeva 2, Mladenovac 11400, Srbija</NuxtLink
+            >
+          </VCardText>
+        </AppBoxBase>
+        <AppBoxBase>
+          <VCardTitle
+            ><IconX
+              icon="mdi:phone"
+              size="2rem"
+              class="text-primary -translate-y-[2px]"
+            />
+            {{ t("Telefon") }}
+          </VCardTitle>
+          <VCardText class="text-body-1"
+            ><a class="text-xl" href="tel:+38163316521">(+381) 63 316 521</a>
+          </VCardText>
+        </AppBoxBase>
+        <AppBoxBase>
+          <VCardTitle
+            ><IconX
+              icon="mdi:at"
+              size="2rem"
+              class="text-primary -translate-y-[2px]"
+            />
+            {{ t("Email") }}
+          </VCardTitle>
+          <VCardText class="text-body-1"
+            ><a class="text-xl" href="mailto:office@metalpromet.rs"
+              >office@metalpromet.rs</a
+            >
+          </VCardText>
+        </AppBoxBase>
+      </AppBoxBase>
     </AppBoxContainerCentered>
   </AppBoxPageWrap>
 </template>
