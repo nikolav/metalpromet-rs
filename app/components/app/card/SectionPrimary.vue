197b7867a43d711622dc922c67953d04203b8634
Vukovic Nikola <admin@nikolav.rs>
2026-08-01 11:52:21 +0200

    Sat, Aug  1, 2026  9:52:21 AM

---
+++ app/components/app/card/SectionPrimary.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+// defineOptions({
+//   name: "COMPONENT_NAME",
+//   inheritAttrs: false,
+// });
+
+// @@eos
+</script>
+
+<template>
+  <VCard
+    tile
+    border="t-md primary-darken-1"
+    class="component--AppCardSectionPrimary"
+  >
+    <VCardTitle
+      class="text-shadow-sm text-center bg-primary-variant position-relative bg-gradient-to-r from-v-primary to-v-primary-variant"
+    >
+      <slot name="title" />
+    </VCardTitle>
+
+    <slot />
+
+    <VCardActions>
+      <slot name="actions" />
+    </VCardActions>
+  </VCard>
+</template>
+
+<!-- scoped component styles -->
+<style lang="scss" scoped></style>
+<!-- css modules, per-class hashing -->
+<style module></style>
+<!-- global styles -->
+<style lang="scss"></style>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -32,7 +32,8 @@ const H = computed(() =>
   <AppBoxPageWrap class="page--index">
     <AppSlidesHero :height="H" />
 
-    <AppBoxContainerCentered class="d-flex flex-col items-center mt-12">
+    <VSpacer class="mt-16" />
+    <AppBoxContainerCentered>
       <VCard
         tile
         border="t-md primary-darken-1"
@@ -70,6 +71,37 @@ const H = computed(() =>
             :class="{ '!grid !grid-cols-[auto_1fr]': d.smAndUp.value }"
           >
             <IconX
+              icon="mdi:factory"
+              size="2rem"
+              class="text-on-surface d-block mx-auto opacity-20"
+              :class="{ 'w-[4rem]': d.smAndUp.value }"
+            />
+            <p>
+              Naša proizvodnja obuhvata kompletne metaloprerađivačke radove
+              &dash; od 3D modelovanja i CNC obrade, preko zavarivanja i montaže
+              čeličnih i aluminijumskih konstrukcija, do vrhunske zaštite
+              metala.
+            </p>
+          </AppBoxBase>
+          <AppBoxBase
+            :class="{ '!grid !grid-cols-[auto_1fr]': d.smAndUp.value }"
+          >
+            <IconX
+              icon="local:shield-improved"
+              size="1.92rem"
+              class="text-on-surface d-block mx-auto opacity-20"
+              :class="{ 'w-[4rem]': d.smAndUp.value }"
+            />
+            <p>
+              Raspolažemo industrijskom komorom za plastifikaciju dužine 7,8
+              metara i visine rama do 2,5m, uz obavezan hemijski predtretman i
+              cink prajmer koji garantuju dugotrajnu korozionu postojanost.
+            </p>
+          </AppBoxBase>
+          <AppBoxBase
+            :class="{ '!grid !grid-cols-[auto_1fr]': d.smAndUp.value }"
+          >
+            <IconX
               icon="mdi:certificate"
               size="2rem"
               class="text-on-surface d-block mx-auto opacity-20"
@@ -93,8 +125,9 @@ const H = computed(() =>
             />
             <p>
               Sa sertifikatom bonitetne izvrsnosti i dokazanim izvoznim
-              iskustvom u Evropi, garantujemo dugotrajnu korozionu postojanost i
-              estetsku završnicu vaših projekata u celoj Srbiji.
+              iskustvom u Evropi (Austrija, Švajcarska i Engleska), garantujemo
+              dugotrajnu korozionu postojanost i estetsku završnicu vaših
+              projekata u celoj Srbiji.
             </p>
           </AppBoxBase>
         </VCardText>
@@ -122,7 +155,75 @@ const H = computed(() =>
           </VBtn>
         </VCardActions>
       </VCard>
-      <VDivider class="mt-12 !border-primary" />
+    </AppBoxContainerCentered>
+
+    <VSpacer class="mt-16" />
+    <AppCardSectionPrimary>
+      <template #title>
+        <IconX
+          v-if="d.smAndUp.value"
+          icon="mdi:package-variant-closed-check"
+          class="position-absolute start-5 z-[1] top-[50%] -translate-y-[50%] opacity-20"
+          size="2rem"
+        />
+        <h2>Kompletno rešenje za metal</h2>
+      </template>
+      <VCardText class="text-body-1 py-6">
+        <VContainer fluid>
+          <VRow>
+            <VCol md="4" class="d-flex flex-col items-center space-y-2">
+              <VAvatar
+                color="rgba(var(--v-theme-primary-variant), .122)"
+                size="4.22rem"
+              >
+                <IconX
+                  icon="local:conveyor-belt-box"
+                  size="2rem"
+                  class="opacity-20"
+                />
+              </VAvatar>
+              <p>
+                Od inženjerske ideje, preko precizne proizvodnje i estetske
+                zaštite, do profesionalne montaže &dash; nudimo vam zatvoren
+                tehnološki ciklus bez outsourcinga.
+              </p>
+            </VCol>
+            <VCol md="4" class="d-flex flex-col items-center space-y-2">
+              <VAvatar
+                color="rgba(var(--v-theme-primary-variant), .122)"
+                size="4.22rem"
+              >
+                <IconX icon="mdi:map-marker" size="2.22rem" class="opacity-20" />
+              </VAvatar>
+              <p>
+                Proizvodna baza u Mladenovcu strateški je pozicionirana za brzu
+                i efikasnu realizaciju projekata u celoj Šumadiji, širem
+                beogradskom regionu, kao i svim opštinama centralne Srbije.
+              </p>
+            </VCol>
+            <VCol md="4" class="d-flex flex-col items-center space-y-2">
+              <VAvatar
+                color="rgba(var(--v-theme-primary-variant), .122)"
+                size="4.22rem"
+              >
+                <IconX
+                  icon="mdi:truck-flatbed"
+                  size="2.5rem"
+                  class="opacity-20"
+                />
+              </VAvatar>
+              <p>
+                Naši montažni timovi su dostupni u svim većim gradovima Srbije,
+                uključujući Beograd, Novi Sad i Niš, kao i u okolnim mestima
+                poput Avale, Kosmaja, Aranđelovca i Sopota.
+              </p>
+            </VCol>
+          </VRow>
+        </VContainer>
+      </VCardText>
+    </AppCardSectionPrimary>
+
+    <AppBoxContainerCentered class="d-flex flex-col items-center">
       <h2 class="text-center">&#64;TODO; sekcije sajta; 🚧</h2>
       <ul>
         <li>✔ HERO</li>
