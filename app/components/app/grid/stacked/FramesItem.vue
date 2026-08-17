90d6a552873be019c8dd17e91b881ddad3f2d300
Vukovic Nikola <admin@nikolav.rs>
2026-08-17 13:50:10 +0200

    Mon, Aug 17, 2026 11:50:10 AM

--- app/components/app/grid/stacked/FramesItem.vue
+++ app/components/app/grid/stacked/FramesItem.vue
@@ -1,28 +1,6 @@
 <script setup lang="ts">
 import type { TAppGridStackedFramesContext } from "~/types";
 
-// defineOptions({
-//   name: "COMPONENT_NAME",
-//   inheritAttrs: false,
-// });
-// definePageMeta({
-//   context: {
-//     title: "pages.NAME.title",
-//     description: "pages.NAME.description",
-//   },
-//   layout: "default",
-//   // middleware: ["auth"],
-//   // validate: (route) => {},
-//   i18n: {
-//     // locales: ["sr", "en"],
-//     paths: {
-//       sr: "/strana/[slug]",
-//       "sr-cyrl": "/strana/[slug]",
-//       en: "/page/[slug]",
-//     },
-//   },
-// });
-
 const context = inject<TAppGridStackedFramesContext>(
   "AppGridStackedFrames_context",
 );
--- app/pages/under-construction/index.vue
+++ app/pages/under-construction/index.vue
@@ -10,17 +10,5 @@ definePageMeta({
 </script>
 
 <template>
-  <AppBoxPageWrap class="page--under-construction">
-    <AppGridStackedFrames class="h-[255px]">
-      <AppGridStackedFramesItem>
-        <NuxtImg
-          src="/logo.jpg"
-          class="w-full fill-height object-cover opacity-20"
-        />
-      </AppGridStackedFramesItem>
-      <AppGridStackedFramesItem>
-        1
-      </AppGridStackedFramesItem>
-    </AppGridStackedFrames>
-  </AppBoxPageWrap>
+  <AppBoxPageWrap class="page--under-construction"> 🚧 </AppBoxPageWrap>
 </template>
